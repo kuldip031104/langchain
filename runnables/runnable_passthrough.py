0d881bcbad0350f1597a991e150e1a046df36104
kuldip <kuldipjadav988@gmail.com>
2026-02-18 18:22:56 +0530

    Added runnable modules

---
+++ runnables/runnable_passthrough.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence,RunnableParallel,RunnablePassthrough
+
+
+load_dotenv()
+
+
+passthrough = RunnablePassthrough()
+
+print(passthrough.invoke(2))
